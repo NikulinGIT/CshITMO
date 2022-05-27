3fee70d161c44eef6f07abdc12f2ce7f1d05edeb
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-05-27 17:22:17 +0300

    Lesson 2

---
+++ DivideIt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first integer");
+            string temp = Console.ReadLine();
+            int i = Int32.Parse(temp);
+
+            Console.WriteLine("Please enter the second integer");
+            temp = Console.ReadLine();
+            int j = Int32.Parse(temp);
+
+        }
+    }
+}
