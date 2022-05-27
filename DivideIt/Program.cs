0d0e117ed9fa162b54061fc457764168e5f5b647
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-05-27 17:23:15 +0300

    lesson3

--- DivideIt/Program.cs
+++ DivideIt/Program.cs
@@ -10,14 +10,25 @@ namespace DivideIt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the first integer");
-            string temp = Console.ReadLine();
-            int i = Int32.Parse(temp);
+            try
+            {
+                Console.WriteLine("Please enter the first integer");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
 
-            Console.WriteLine("Please enter the second integer");
-            temp = Console.ReadLine();
-            int j = Int32.Parse(temp);
+                Console.WriteLine("Please enter the second integer");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
 
+                int k = i / j;
+                Console.WriteLine("The result of dividing {0} by {1} is {2}", i, j, k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e);
+            }
+            Console.Read();
         }
+    }
     }
 }
