02724674fcc9ea82491054f8056038e8dad7078b
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-05-30 19:27:08 +0300

    lesson2

--- лаб2/StructType/Struct.cs
+++ лаб2/StructType/Struct.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Struct
+    {
+        public enum AccountType
+        {
+            Checking,
+            Deposit
+        }
+
+        public struct BankAccount
+        {
+            public long accNo;
+            public decimal accBal;
+            public AccountType accType;
+        }
+
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = 3200.00M;
+            goldAccount.accNo = 123;
+            goldAccount.accNo = long.Parse(Console.ReadLine());
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Acct Number {0}", goldAccount.accNo);
+            Console.WriteLine("Acct Type {0}", goldAccount.accType);
+            Console.WriteLine("Acct Balance ${0}",goldAccount.accBal);
+            Console.Read();
+        }
+    }
+}
