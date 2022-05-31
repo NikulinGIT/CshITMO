5d404ae297ebe6ef4d46d1209f67fde7211ce4a8
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-05-31 11:41:35 +0300

    lesson1

--- лаб3/WhatDay1/Program.cs
+++ лаб3/WhatDay1/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace WhatDay
+class WhatDay
 {
-    class Struct
+    static void Main()
     {
-       
-
-    
-        static void Main(string[] args)
-        {
-            
-        }
+        Console.Write("Please enter a day number between 1 and 365: ");
+        string line = Console.ReadLine();
+        int dayNum = int.Parse(line);
     }
+
 }
