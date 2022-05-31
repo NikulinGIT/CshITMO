ea4f26ff8506f1c97fc6856737570beae8dad841
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-05-31 12:15:55 +0300

    lesson2

---
+++ лаб3/WhatDay2/WhatDay2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+enum MonthName
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+class WhatDay
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Please enter a day number between 1 and 365: ");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+
+            if (dayNum < 1 || dayNum > 365)
+            {
+                throw new ArgumentOutOfRangeException("Day out of range");
+            }
+
+            int monthNum = 0;
+            int[] DaysInMonths = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+            foreach (int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+            Console.WriteLine("{0} {1}", dayNum, monthName);
+        }
+        catch (Exception caught)
+        {
+            Console.WriteLine("Неправильный формат");
+
+
+        }
+
+    }
+}
