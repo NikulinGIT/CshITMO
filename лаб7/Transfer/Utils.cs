fd5ec0345f7c5e7e48eeebab343e32127e962157
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-05-31 19:38:46 +0300

    lesson2

---
+++ лаб7/Transfer/Utils.cs
@@ -0,0 +1,27 @@
+﻿namespace Transfer
+{
+    using System;
+
+    class Utils
+    {
+
+        //As before
+        // Reverse a string
+
+        public static void Reverse(ref string s)
+        {
+            string sRev = "";
+
+            for (int k = s.Length-1; k >= 0 ; k--)
+              {
+                sRev = sRev + s[k];
+            }
+            // Return result to caller
+            s = sRev;
+            
+        }
+    }
+
+
+}
+
--- лаб7/Transfer/test.cs
+++ лаб7/Transfer/test.cs
@@ -6,21 +6,32 @@ namespace Transfer
     {
         public static void Main()
         {
-            BankAccount b1 = new BankAccount();
-            b1.Populate(100);
+            //BankAccount b1 = new BankAccount();
+            //b1.Populate(100);
 
-            BankAccount b2 = new BankAccount();
-            b2.Populate(100);
+            //BankAccount b2 = new BankAccount();
+            //b2.Populate(100);
 
-            Console.WriteLine("Before transfer");
-            Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
-            Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+            //Console.WriteLine("Before transfer");
+            //Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
+            //Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
 
-            b1.TransferFrom(b2, 10);
+            //b1.TransferFrom(b2, 10);
 
-            Console.WriteLine("After transfer");
-            Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
-            Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+            //Console.WriteLine("After transfer");
+            //Console.WriteLine("{0} {1} {2}", b1.Type(), b1.Number(), b1.Balance());
+            //Console.WriteLine("{0} {1} {2}", b2.Type(), b2.Number(), b2.Balance());
+            string message;
+
+            // Get an input string
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            // Reverse the string
+            Utils.Reverse(ref message);
+
+            // Display the result
+            Console.WriteLine(message);
 
         }
     }
