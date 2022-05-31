2f02e8b934fbe085806ce47e99dee5834238d325
Nikulin Ivan <103510364+NikulinGIT@users.noreply.github.com>
2022-05-31 12:08:12 +0300

    lesson1_2

--- лаб3/WhatDay1/Program.cs
+++ лаб3/WhatDay1/Program.cs
@@ -23,115 +23,21 @@ class WhatDay
         string line = Console.ReadLine();
         int dayNum = int.Parse(line);
         int monthNum = 0;
-        if (dayNum <= 31)
-        { // January
-            goto End;
-        }
-        else
+        int[] DaysInMonths = {31,28,31,30,31, 30, 31, 30, 31, 30, 31, 30 };
+        int i = 0;
+        foreach (int dayInMonth in DaysInMonths)
         {
-            dayNum -= 31;
-            monthNum++;
+            if (dayNum <= dayInMonth)
+            {
+                break;
+            }
+            else
+            {
+                dayNum -= dayInMonth;
+                monthNum++;
+            }
         }
-        if (dayNum <= 28)
-        { // February
-            goto End;
-        }
-        else
-        {
-            dayNum -= 28;
-            monthNum++;
-        }
-        if (dayNum <= 31)
-        { // March
-            goto End;
-        }
-        else
-        {
-            dayNum -= 31;
-            monthNum++;
-        }
-        if (dayNum <= 30)
-        { // April
-            goto End;
-        }
-        else
-        {
-            dayNum -= 30;
-            monthNum++;
-        }
-        if (dayNum <= 31)
-        { // May
-            goto End;
-        }
-        else
-        {
-            dayNum -= 31;
-            monthNum++;
-        }
-        if (dayNum <= 30)
-        { // June
-            goto End;
-        }
-        else
-        {
-            dayNum -= 30;
-            monthNum++;
-        }
-        if (dayNum <= 31)
-        { // July
-            goto End;
-        }
-        else
-        {
-            dayNum -= 31;
-            monthNum++;
-        }
-        if (dayNum <= 31)
-        { // August
-            goto End;
-        }
-        else
-        {
-            dayNum -= 31;
-            monthNum++;
-        }
-        if (dayNum <= 30)
-        { // September
-            goto End;
-        }
-        else
-        {
-            dayNum -= 30;
-            monthNum++;
-        }
-        if (dayNum <= 31)
-        { // October
-            goto End;
-        }
-        else
-        {
-            dayNum -= 31;
-            monthNum++;
-        }
-        if (dayNum <= 30)
-        { // November
-            goto End;
-        }
-        else
-        {
-            dayNum -= 30;
-            monthNum++;
-        }
-        if (dayNum <= 31)
-        { // December
-            goto End;
-        }
-        else
-        {
-            dayNum -= 31;
-            monthNum++;
-        }
-    End:
+
         MonthName temp = (MonthName)monthNum;
         string monthName = temp.ToString();
 
